2059eaca835c77ac5b10a380a5155589575e88ae
rpazos-h <68419585+rpazos-h@users.noreply.github.com>
2021-07-03 21:31:00 +0200

    Update getEPH.R

--- getEPH.R
+++ getEPH.R
@@ -1,4 +1,4 @@
-getEPHS <- function(years=c(2003:2020),
+getEPHS <- function(years=c(2003:2013),
                     trims=c(1:4),
                     ind=TRUE,
                     dir=NULL,
@@ -7,7 +7,7 @@ getEPHS <- function(years=c(2003:2020),
   if (!is.loaded("data.table")) library(data.table)
   if (!is.loaded("foreign")) library(foreign)
   
-  ephsDisponibles <- data.table(year=c(rep(2003,2),rep(2004:2006,each=4),rep(2007,3),rep(2008:2014,each=4),rep(2015,2),rep(2016,3),rep(2017:2018,each=4),rep(2019,4),rep(2020,4)),
+  ephsDisponibles <- data.table(year=c(rep(2003,2),rep(2004:2006,each=4),rep(2007,3),rep(2008:2013,each=4)),
                                 trim=c(3,4,rep(1:4,3),1,2,4,rep(1:4,7),1,2,2,3,4,rep(1:4,2),1,2,3,4,1,2,3,4),
                                 linkDescarga=c('https://www.indec.gob.ar/ftp/cuadros/menusuperior/eph/t303_dta.zip',
                                                'https://www.indec.gob.ar/ftp/cuadros/menusuperior/eph/t403_dta.zip',
